375981c648759796a9f966bd65c2c444367d7b64
nlapi <nisjet.lapi@gmail.com>
2025-07-10 17:52:35 -0600

    Add Spacing Between Logo Parts

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import { supabase } from '../utils/supabase'
 
 Vue.use(VueRouter)
 
@@ -9,63 +8,20 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: HomeView,
-        meta: { requiresAuth: true }
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('../views/Login.vue'),
-        meta: { requiresGuest: true }
-    },
-    {
-        path: '/signup',
-        name: 'signup',
-        component: () => import('../views/Signup.vue'),
-        meta: { requiresGuest: true }
-    },
-    {
-        path: '/pricing',
-        name: 'pricing',
-        component: () => import('../views/Pricing.vue'),
-        meta: { requiresAuth: true }
-    },
-    {
-        path: '/success',
-        name: 'success',
-        component: () => import('../views/Success.vue'),
-        meta: { requiresAuth: true }
+        component: HomeView
     },
     {
         path: '/setting',
-        name: 'setting',
-        component: () => import('../views/Setting.vue'),
-        meta: { requiresAuth: true }
+        component: () => import(/* webpackChunkName: "about" */ '../views/Setting.vue')
     },
     {
         path: '/customize',
-        name: 'customize',
-        component: () => import('../views/Customize.vue'),
-        meta: { requiresAuth: true }
+        component: () => import(/* webpackChunkName: "customize" */ '../views/Customize.vue')
     },
 ]
 
 const router = new VueRouter({
     routes
-})
-
-// Navigation guard
-router.beforeEach(async (to, from, next) => {
-    const { data: { session } } = await supabase.auth.getSession()
-    const isAuthenticated = !!session
-
-    if (to.meta.requiresAuth && !isAuthenticated) {
-        next('/login')
-    } else if (to.meta.requiresGuest && isAuthenticated) {
-        next('/')
-    } else {
-        next()
-    }
 })
 
 export default router
