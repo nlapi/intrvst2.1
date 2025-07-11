bd094bed1d4b330cb6c65883778dabfc8bc3e7de
nlapi <nisjet.lapi@gmail.com>
2025-07-11 01:07:33 -0600

    Fix Asset Loading and WebSocket Connection Errors

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '' // 设置publicPath为空字符串
+  publicPath: '/' // 设置publicPath为根路径以确保正确的资源加载
 })
